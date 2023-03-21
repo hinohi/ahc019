284b4f6b333ec2bf0892e67cc0690bd762152ce0
Nakayama Daiju <42.daiju@gmail.com>
2023-03-21 17:55:41 +0900

    StepFunctions で並列実行することにする

--- cdk/bin/main.ts
+++ cdk/bin/main.ts
@@ -1,13 +1,16 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sfn from "aws-cdk-lib/aws-stepfunctions";
+import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
+import * as logs from "aws-cdk-lib/aws-logs";
 import * as path from "path";
 
 export class Ahc019 extends cdk.Stack {
   constructor(scope: cdk.App, id: string) {
     super(scope, id);
 
-    new lambda.DockerImageFunction(this, "Function", {
+    const lambdaFunction = new lambda.DockerImageFunction(this, "Function", {
       functionName: "ahc019",
       memorySize: 1792,
       timeout: cdk.Duration.seconds(10),
@@ -17,6 +20,31 @@ export class Ahc019 extends cdk.Stack {
           file: "Dockerfile",
         }
       ),
+    });
+
+    const log = new logs.LogGroup(this, 'LogGroup', {
+      logGroupName: 'ahc019',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const task = new tasks.LambdaInvoke(this, "Invoke", {
+      lambdaFunction,
+      outputPath: '$.Payload',
+    });
+    const map = new sfn.Map(this, "Map");
+    map.iterator(task);
+    new sfn.StateMachine(this, "StateMachine", {
+      stateMachineName: "AHC019",
+      stateMachineType: sfn.StateMachineType.STANDARD,
+      definition: map,
+      timeout: cdk.Duration.hours(1),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tracingEnabled: false,
+      logs: {
+        destination: log,
+        level: sfn.LogLevel.ERROR,
+        includeExecutionData: true,
+      },
     });
   }
 }
---
+++ run_many.py
@@ -0,0 +1,81 @@
+import json
+import argparse
+import uuid
+import time
+from pathlib import Path
+from datetime import datetime
+import math
+
+import boto3
+
+ARN = 'arn:aws:states:ap-northeast-1:169698630369:stateMachine:AHC019'
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('-n', type=int, required=True)
+    p.add_argument('-d', type=int, required=True)
+    p.add_argument('--max-step', '-S', type=int, required=True)
+    p.add_argument('--max-temperature', '-T', type=float, required=True)
+    p.add_argument('--min-temperature', type=float, default=1e-4)
+    args = p.parse_args()
+
+    tasks = []
+    for seed in range(args.n):
+        tasks.append({
+            'seed': seed + 1,
+            'd': args.d,
+            'max_step': args.max_step,
+            'max_temperature': args.max_temperature,
+            'min_temperature': args.min_temperature,
+        })
+
+    client = boto3.client('stepfunctions')
+    name = str(uuid.uuid4())
+    r = client.start_execution(
+        stateMachineArn=ARN,
+        name=name,
+        input=json.dumps(tasks),
+    )
+    execution_arn = r['executionArn']
+    print(execution_arn)
+    time.sleep(7)
+    while True:
+        out = client.describe_execution(executionArn=execution_arn)
+        match out['status']:
+            case 'SUCCEEDED':
+                summary_score(name, json.loads(out['output']))
+                break
+            case 'FAILED' | 'TIMED_OUT' | 'ABORTED':
+                print(out)
+                break
+            case 'RUNNING':
+                time.sleep(1)
+            case s:
+                print(s)
+
+
+def summary_score(name: str, outputs: list[dict]):
+    base = Path(__file__).parent / 'exec_log'
+    base.mkdir(parents=True, exist_ok=True)
+    fname = f'{datetime.now():%m%d_%H%M%S}_{name}.json'
+    with (base / fname).open('w') as f:
+        json.dump(outputs, f, indent=2)
+
+    params = outputs[0]['request']
+    d = params['d']
+    n = len(outputs)
+
+    s = 0.0
+    log_s = 0.0
+    for o in outputs:
+        score = o['score']
+        s += score
+        log_s += math.log(score)
+    s /= n
+    log_s /= n
+    print(f'{d} {n} {s} {log_s}')
+
+
+if __name__ == '__main__':
+    main()
