d6cd73ca02893de7b5bf62bb70409511a82fe894
Nakayama Daiju <42.daiju@gmail.com>
2023-03-25 09:15:54 +0900

    リファクタリング

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 // pub mod brute_force;
+mod block_set;
 mod grid;
 mod mc;
 mod solver;
 
+pub use block_set::*;
 pub use grid::*;
 pub use mc::*;
 pub use solver::*;
--- src/solver.rs
+++ src/solver.rs
@@ -1,6 +1,5 @@
-use crate::{AxisMap, Grid3, GridFront, GridRight, McParams, Point};
-use rand::seq::SliceRandom;
-use rand::Rng;
+use crate::{AxisMap, BlockSet, Grid3, GridFront, GridRight, McParams, Point};
+use rand::{seq::SliceRandom, Rng};
 use rand_pcg::Mcg128Xsl64;
 use std::time::{Duration, Instant};
 
@@ -16,15 +15,6 @@ pub struct YetPointSet {
     yet_yet: Vec<Point>,
     yet: Vec<Point>,
     can: Vec<Point>,
-}
-
-#[derive(Clone)]
-pub struct Block {
-    shared: Vec<(u16, Vec<Point>, Vec<Point>)>,
-    shared_id_stock: Vec<u16>,
-    half1: Vec<Point>,
-    half2: Vec<Point>,
-    next_half_id: u16,
 }
 
 pub fn make_face(shadow: &[Vec<u8>], t: bool) -> Vec<u8> {
@@ -153,78 +143,6 @@ impl YetPointSet {
     }
 }
 
-impl Block {
-    pub fn new() -> Block {
-        Block {
-            shared: Vec::new(),
-            shared_id_stock: Vec::new(),
-            half1: Vec::new(),
-            half2: Vec::new(),
-            next_half_id: 10000,
-        }
-    }
-
-    pub fn shared_only_score(&self) -> f64 {
-        let mut score = 0.0;
-        for (_, v, _) in self.shared.iter() {
-            score += 1.0 / v.len() as f64;
-        }
-        score
-    }
-
-    pub fn gen_shared_block_id(&mut self) -> u16 {
-        if let Some(id) = self.shared_id_stock.pop() {
-            id
-        } else {
-            (self.shared.len() + 1) as u16
-        }
-    }
-
-    pub fn gen_half_block_id(&mut self) -> u16 {
-        let id = self.next_half_id;
-        self.next_half_id += 1;
-        id
-    }
-
-    pub fn push_shared(&mut self, block_id: u16, p1: Vec<Point>, p2: Vec<Point>) {
-        self.shared.push((block_id, p1, p2));
-    }
-
-    fn pop_shared(&mut self, i: usize) -> (Vec<Point>, Vec<Point>) {
-        let (id, s1, s2) = self.shared.swap_remove(i);
-        self.shared_id_stock.push(id);
-        (s1, s2)
-    }
-
-    pub fn pop_random(&mut self, rng: &mut Mcg128Xsl64) -> (Vec<Point>, Vec<Point>) {
-        let i = rng.gen_range(0, self.shared.len());
-        self.pop_shared(i)
-    }
-
-    pub fn pop_small(&mut self, th: usize) -> Option<(Vec<Point>, Vec<Point>)> {
-        for i in 0..self.shared.len() {
-            if self.shared[i].1.len() <= th {
-                return Some(self.pop_shared(i));
-            }
-        }
-        None
-    }
-
-    pub fn push_half(&mut self, place: u8, p: Point) {
-        if place == 1 {
-            self.half1.push(p);
-        } else {
-            self.half2.push(p);
-        }
-    }
-
-    pub fn half_reset(&mut self) {
-        self.half1.clear();
-        self.half2.clear();
-        self.next_half_id = 10000;
-    }
-}
-
 fn grow_shared_block(
     rng: &mut Mcg128Xsl64,
     grid_1: &mut GridBox,
@@ -275,10 +193,10 @@ fn fill_all(
     hole_2: &[(u8, u8, Vec<u8>)],
     grid_1: &mut GridBox,
     grid_2: &mut GridBox,
-    block: &mut Block,
+    block: &mut BlockSet,
     cut_off: f64,
 ) -> Option<f64> {
-    fn single_update_loop(grid: &mut GridBox, p: Point, block: &mut Block, place: u8) -> f64 {
+    fn single_update_loop(grid: &mut GridBox, p: Point, block: &mut BlockSet, place: u8) -> f64 {
         let block_id = block.gen_half_block_id();
         let mut c = 0;
         let mut stack = vec![p];
@@ -344,7 +262,7 @@ pub fn mc_run(
     let mut grid_2 = GridBox::new(d, &front2, right2);
     let hole_1 = grid_1.make_hole_xzy();
     let hole_2 = grid_2.make_hole_xzy();
-    let mut block = Block::new();
+    let mut block = BlockSet::new();
     let mut best = loop {
         if let Some(score) = fill_all(
             rng,
@@ -359,7 +277,7 @@ pub fn mc_run(
         }
         grid_1 = GridBox::new(d, &front1, right1);
         grid_2 = GridBox::new(d, &front2, right2);
-        block = Block::new();
+        block = BlockSet::new();
     };
     let mut score = best.2;
     for step in 0..params.max_step {
